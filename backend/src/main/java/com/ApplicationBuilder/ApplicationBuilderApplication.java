f4bf4d674404451360d895dc814da05f61416d27
Leon Beitz <leonbeitz@Leons-MacBook-Air.local>
2025-08-24 15:06:27 +0200

    chore: initial clean repo setup

---
+++ backend/src/main/java/com/ApplicationBuilder/ApplicationBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.ApplicationBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBuilderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ApplicationBuilder/ApplicationBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApplicationBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
